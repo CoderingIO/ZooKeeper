a370e0dce0ebdc95cd772d944c1742cc594dbf44
Wayne Bangert <poncho4242@yahoo.com>
2016-02-15 16:20:23 -0700

    need to finish work on staff view controller

---
+++ ZooKeeper/Staff.swift
@@ -0,0 +1,9 @@
+//
+//  Staff.swift
+//  ZooKeeper
+//
+//  Created by Wayne Bangert on 2/11/16.
+//  Copyright © 2016 Wayne Bangert. All rights reserved.
+//
+
+import Foundation
---
+++ ZooKeeper/StaffTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StaffTableViewCell.swift
+//  ZooKeeper
+//
+//  Created by Wayne Bangert on 2/15/16.
+//  Copyright © 2016 Wayne Bangert. All rights reserved.
+//
+
+import UIKit
+
+class StaffTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
