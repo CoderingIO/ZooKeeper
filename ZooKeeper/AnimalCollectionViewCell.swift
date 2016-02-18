b16b54b6a0f24c237ee444fa3a4f1141324ed844
Wayne Bangert <poncho4242@yahoo.com>
2016-02-17 21:51:52 -0700

    work from wed class

---
+++ ZooKeeper/AnimalCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AnimalCollectionViewCell.swift
+//  ZooKeeper
+//
+//  Created by Wayne Bangert on 2/17/16.
+//  Copyright © 2016 Wayne Bangert. All rights reserved.
+//
+
+import UIKit
+
+class AnimalCollectionViewCell: UICollectionViewCell {
+    
+}
